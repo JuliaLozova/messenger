ccd761df2cca9f5f56907746f6f85cd3428c5f83
Julia <jlozova12@gmail.com>
2017-06-26 21:52:52 -0700

    MessengerTerminal now Server script

--- MessengerTerminal.py
+++ MessengerTerminal.py
@@ -1,16 +1,32 @@
-import sys
 import random
+import socket 
 
-answers = [
-    "Hello Stranger",
-    "Its been a while",
-    "Awesome",
-    "YAY!"
-]
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50007              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("Welcome to Jchat")
-print("Say 'Bye' to end chat")
-userInput = raw_input("Let's Chat!\n")
-while userInput != "Bye":
-    print random.choice(answers)
-    userInput = raw_input("")
+s.bind((HOST, PORT))
+
+s.listen(1)
+conn, addr = s.accept()
+print 'Connected by', addr
+while 1:
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
+conn.close()
+#
+#answers = [
+#    "Hello Stranger",
+#    "Its been a while",
+#    "Awesome",
+#    "YAY!"
+#]
+#
+#print("Welcome to Jchat")
+#print("Say 'Bye' to end chat")
+#userInput = raw_input("Let's Chat!\n")
+#while userInput != "Bye":
+#    print random.choice(answers)
+#    userInput = raw_input("")
+#
