59465c4879cd4ab7f53382be9d0d869fd68e7977
Julia <jlozova12@gmail.com>
2017-06-27 19:58:34 -0700

    MessengerTerminal is now Server to communicate with MessengerClient

--- MessengerClient.py
+++ MessengerClient.py
@@ -1,10 +1,17 @@
 import socket
+import sys
 
 HOST = ''                 # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
-data = s.recv(1024)
+
+message = raw_input("-->>")
+
+while message != "Bye":
+    
+    s.send(message)
+    data = s.recv(1024)
+    print 'Recieved-->', repr(data)
+    message = raw_input("-->>")
 s.close()
-print 'Received', repr(data)
--- MessengerTerminal.py
+++ MessengerTerminal.py
@@ -1,19 +1,22 @@
 import random
 import socket 
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
+HOST = ''                 
+PORT = 50007              
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind((HOST, PORT))
 
 s.listen(1)
 conn, addr = s.accept()
+
 print 'Connected by', addr
 while 1:
     data = conn.recv(1024)
     if not data: break
-    conn.sendall(data)
+    print 'Recieved-->', repr(data)
+    data = raw_input("-->")
+    conn.send(data)
 conn.close()
 #
 #answers = [
